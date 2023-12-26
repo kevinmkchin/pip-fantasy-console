630d2db40217adbe91fbff58baeede231367adb8
Kevin Chin <44921110+kevinmkchin@users.noreply.github.com>
2023-12-25 21:43:57 -0500

    VM

--- code/PipLang.cpp
+++ code/PipLang.cpp
@@ -8,7 +8,12 @@ enum class OpCode : u8
 {
     RETURN,
     CONSTANT,
-    CONSTANT_LONG
+    CONSTANT_LONG,
+    NEGATE,
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
 };
 
 struct TValue
@@ -34,18 +39,36 @@ void InitChunk(Chunk *chunk)
     chunk->constants = new std::vector<TValue>();
 }
 
-void WriteChunk(Chunk *chunk, u8 byte, int line)
-{
-    chunk->linenumbers->push_back(line);
-    chunk->bytecode->push_back(byte);
-}
-
 void FreeChunk(Chunk *chunk)
 {
     chunk->linenumbers->clear();
     chunk->bytecode->clear();
     chunk->constants->clear();
     // Note(Kevin): I'm choosing not to delete the std::vectors. Keep them alive but just empty.
+}
+
+void WriteChunk(Chunk *chunk, u8 byte, int line)
+{
+    chunk->linenumbers->push_back(line);
+    chunk->bytecode->push_back(byte);
+}
+
+void WriteConstant(Chunk *chunk, TValue value, int line)
+{
+    chunk->constants->push_back(value);
+    u32 cindex = (u32)chunk->constants->size() - 1;
+    if (cindex >= 256)
+    {
+        WriteChunk(chunk, (u8)OpCode::CONSTANT_LONG, line);
+        WriteChunk(chunk, (u8)(cindex >> 16), line);
+        WriteChunk(chunk, (u8)(cindex >> 8), line);
+        WriteChunk(chunk, (u8)cindex, line);
+    }
+    else
+    {
+        WriteChunk(chunk, (u8)OpCode::CONSTANT, line);
+        WriteChunk(chunk, (u8)cindex, line);
+    }
 }
 
 void PrintTValue(TValue value)
@@ -96,12 +119,22 @@ int Debug_DisassembleInstruction(Chunk *chunk, int offset)
     OpCode instruction = (OpCode)chunk->bytecode->at(offset);
     switch (instruction)
     {
-    case OpCode::CONSTANT_LONG:
-        return Debug_ConstantLongInstruction("CONSTANT_LONG", chunk, offset);
-    case OpCode::CONSTANT:
-        return Debug_ConstantInstruction("CONSTANT", chunk, offset);
     case OpCode::RETURN:
         return Debug_SimpleInstruction("RETURN", offset);
+    case OpCode::CONSTANT:
+        return Debug_ConstantInstruction("CONSTANT", chunk, offset);
+    case OpCode::CONSTANT_LONG:
+        return Debug_ConstantLongInstruction("CONSTANT_LONG", chunk, offset);
+    case OpCode::NEGATE:
+        return Debug_SimpleInstruction("NEGATE", offset);
+    case OpCode::ADD:
+        return Debug_SimpleInstruction("ADD", offset);
+    case OpCode::SUBTRACT:
+        return Debug_SimpleInstruction("SUBTRACT", offset);
+    case OpCode::MULTIPLY:
+        return Debug_SimpleInstruction("MULTIPLY", offset);
+    case OpCode::DIVIDE:
+        return Debug_SimpleInstruction("DIVIDE", offset);
     default:
         printf("Unknown opcode %d\n", instruction);
         return offset + 1;
@@ -119,35 +152,185 @@ void Debug_DisassembleChunk(Chunk *chunk, const char *name)
 }
 
 
-void WriteConstant(Chunk *chunk, TValue value, int line)
+/// VM
+
+#define STACK_MAX 4000
+
+struct VM
 {
-    chunk->constants->push_back(value);
-    u32 cindex = chunk->constants->size() - 1;
-    if (cindex >= 256)
-    {
-        WriteChunk(chunk, (u8)OpCode::CONSTANT_LONG, line);
-        WriteChunk(chunk, (u8)cindex >> 16, line);
-        WriteChunk(chunk, (u8)cindex >> 8, line);
-        WriteChunk(chunk, (u8)cindex, line);
-    }
-    else
-    {
-        WriteChunk(chunk, (u8)OpCode::CONSTANT, line);
-        WriteChunk(chunk, (u8)cindex, line);
-    }
+    Chunk *chunk;
+    u8 *ip; // instruction pointer
+    TValue stack[STACK_MAX];
+    TValue *sp; // stack pointer
+};
+
+VM vm;
+
+void Stack_Reset()
+{
+    vm.sp = vm.stack;
 }
+
+void Stack_Push(TValue value)
+{
+    *vm.sp = value;
+    ++vm.sp;
+}
+
+TValue Stack_Pop()
+{
+    --vm.sp;
+    return *vm.sp;
+}
+
+void InitVM()
+{
+    Stack_Reset();
+}
+
+void FreeVM()
+{
+
+}
+
+enum class InterpretResult 
+{
+    OK,
+    COMPILE_ERROR,
+    RUNTIME_ERROR
+};
+
+#define DEBUG_TRACE_EXECUTION
+
+static InterpretResult Run()
+{
+#define VM_READ_BYTE() (*vm.ip++) // read byte and move pointer along
+#define VM_READ_CONSTANT() (vm.chunk->constants->at(VM_READ_BYTE()))
+#define VM_READ_CONSTANT_LONG() (vm.chunk->constants->at(VM_READ_BYTE() << 16 | VM_READ_BYTE() << 8 | VM_READ_BYTE()))
+#define VM_BINARY_OP(op) \
+    do { \
+      double r = Stack_Pop().real; \
+      double l = Stack_Pop().real; \
+      TValue v; \
+      v.real = l op r; \
+      Stack_Push(v); \
+    } while (false)
+
+
+    for (;;)
+    {
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (TValue *slot = vm.stack; slot < vm.sp; ++slot)
+        {
+            printf("[ ");
+            PrintTValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        Debug_DisassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->bytecode->data()));
+#endif
+        OpCode op;
+        switch (op = (OpCode)VM_READ_BYTE())
+        {
+
+            case OpCode::RETURN:
+            {
+                PrintTValue(Stack_Pop());
+                printf("\n");
+                return InterpretResult::OK;
+            }
+
+            case OpCode::CONSTANT:
+            {
+                TValue constant = VM_READ_CONSTANT();
+                Stack_Push(constant);
+                break;
+            }
+
+            case OpCode::CONSTANT_LONG:
+            {
+                TValue constant = VM_READ_CONSTANT_LONG();
+                Stack_Push(constant);
+                break;
+            }
+
+            case OpCode::NEGATE:
+            {
+                TValue v = Stack_Pop();
+                v.real = -v.real;
+                Stack_Push(v);
+                break;
+            }
+
+            case OpCode::ADD: VM_BINARY_OP(+); break;
+            case OpCode::SUBTRACT: VM_BINARY_OP(-); break;
+            case OpCode::MULTIPLY: VM_BINARY_OP(*); break;
+            case OpCode::DIVIDE: VM_BINARY_OP(/); break;
+
+        }
+    }
+
+
+#undef VM_READ_BYTE
+#undef VM_READ_CONSTANT
+#undef VM_READ_CONSTANT_LONG
+#undef VM_BINARY_OP
+}
+
+InterpretResult Interpret(Chunk *chunk)
+{
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->bytecode->data();
+    return Run();
+}
+
+
+
+
+
 
 void PipLangRunSomeThings()
 {
+    InitVM();
+
     Chunk chunk;
     InitChunk(&chunk);
 
-    TValue v;
-    v.real = 34.201;
-    WriteConstant(&chunk, v, 0);
-    WriteConstant(&chunk, v, 1);
-    WriteChunk(&chunk, (u8)OpCode::RETURN, 2);
+    //TValue v;
+    //v.real = 34.201;
+    //WriteConstant(&chunk, v, 0);
+    //WriteConstant(&chunk, v, 1);
+    //WriteChunk(&chunk, (u8)OpCode::NEGATE, 1);
+    //WriteChunk(&chunk, (u8)OpCode::RETURN, 2);
 
-    Debug_DisassembleChunk(&chunk, "test chunk");
+    TValue a, b, c;
+    a.real = 1.2;
+    b.real = 3.4;
+    c.real = 5.6;
+
+    WriteConstant(&chunk, a, 0);
+    WriteConstant(&chunk, b, 0);
+    WriteChunk(&chunk, (u8)OpCode::ADD, 0);
+    WriteConstant(&chunk, c, 0);
+    WriteChunk(&chunk, (u8)OpCode::DIVIDE, 0);
+    WriteChunk(&chunk, (u8)OpCode::NEGATE, 0);
+    WriteChunk(&chunk, (u8)OpCode::RETURN, 1);
+
+    //Debug_DisassembleChunk(&chunk, "test chunk");
+    Interpret(&chunk);
+
+    FreeVM();
     FreeChunk(&chunk);
 }
+
+
+
+
+
+
+
+
+
+
+
