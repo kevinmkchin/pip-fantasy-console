9e9ee147ce2c280a6a39b4e464744b6612f2b775
Kevin Chin <44921110+kevinmkchin@users.noreply.github.com>
2024-03-10 16:45:54 -0400

    intertwine mouse positions Bresenham

--- code/editor/SpriteEditor.cpp
+++ code/editor/SpriteEditor.cpp
@@ -94,6 +94,122 @@ struct Fuck
     spredit_Color pixel;
 };
 
+
+static int brushSz = 1;
+static vec3 activeRGB = vec3();
+static float activeOpacity = 1.f;
+std::vector<Fuck> restore;
+
+void DoBrushPoint(i32 x, i32 y)
+{
+    float brushRadius = (float)brushSz/2.f;
+    i32 mx = x - brushSz/2;
+    i32 my = y - brushSz/2;
+
+    for (int i = 0; i < brushSz; ++i)
+    {
+        for (int j = 0; j < brushSz; ++j)
+        {
+            float fx = (float)x;
+            float fy = (float)y;
+            if (brushSz % 2 == 1)
+            {
+                fx += 0.5f;
+                fy += 0.5f;
+            }
+            float cx = -brushRadius + (float)i + 0.5f;
+            float cy = -brushRadius + (float)j + 0.5f;
+
+            if (sqrtf(float(cx*cx + cy*cy)) < float(brushSz/2) + (brushSz % 2 == 1 ? 0.24f : -0.1f))
+            {
+                spredit_Color *p = PixelAt(&spreditState.frame, mx+i, my+j);
+                if (p)
+                {
+                    // Blend mode: RGB is linearly interpolated, A is added onto where opacity is % from
+                    // existing alpha to 255.
+                    vec3 existingRGB = {RGB255TO1(p->r, p->g, p->b)};
+                    vec3 finalRGB = Lerp(existingRGB, activeRGB, activeOpacity);
+                    restore.push_back({ mx+i, my+j, *p });
+                    p->r = u8(finalRGB.x * 255.f);
+                    p->g = u8(finalRGB.y * 255.f);
+                    p->b = u8(finalRGB.z * 255.f);
+                    p->a = GM_clamp(p->a + u8(float(255 - p->a) * activeOpacity), 0, 255);
+                }
+            }
+        }
+    }
+}
+
+// Line code based on Alois Zingl work released under the
+// MIT license http://members.chello.at/easyfilter/bresenham.html
+void algo_line_continuous(int x0, int y0, int x1, int y1)
+{
+  int dx =  GM_abs(x1-x0), sx = (x0 < x1 ? 1: -1);
+  int dy = -GM_abs(y1-y0), sy = (y0 < y1 ? 1: -1);
+  int err = dx+dy, e2;                                  // error value e_xy
+
+  for (;;) {
+    DoBrushPoint(x0, y0);
+    e2 = 2*err;
+    if (e2 >= dy) {                                       // e_xy+e_x > 0
+      if (x0 == x1)
+        break;
+      err += dy;
+      x0 += sx;
+    }
+    if (e2 <= dx) {                                       // e_xy+e_y < 0
+      if (y0 == y1)
+        break;
+      err += dx;
+      y0 += sy;
+    }
+  }
+}
+
+void algo_line_perfect(int x1, int y1, int x2, int y2) // I don't like
+{
+  bool yaxis;
+
+  // If the height if the line is bigger than the width, we'll iterate
+  // over the y-axis.
+  if (GM_abs(y2-y1) > GM_abs(x2-x1)) {
+    std::swap(x1, y1);
+    std::swap(x2, y2);
+    yaxis = true;
+  }
+  else
+    yaxis = false;
+
+  const int w = GM_abs(x2-x1)+1;
+  const int h = GM_abs(y2-y1)+1;
+  const int dx = GM_sign(x2-x1);
+  const int dy = GM_sign(y2-y1);
+
+  int e = 0;
+  int y = y1;
+
+  // Move x2 one extra pixel to the dx direction so we can use
+  // operator!=() instead of operator<(). Here I prefer operator!=()
+  // instead of swapping x1 with x2 so the error always start from 0
+  // in the origin (x1,y1).
+  x2 += dx;
+
+  for (int x=x1; x!=x2; x+=dx) {
+    if (yaxis)
+      DoBrushPoint(y, x);
+    else
+      DoBrushPoint(x, y);
+
+    // The error advances "h/w" per each "x" step. As we're using a
+    // integer value for "e", we use "w" as the unit.
+    e += h;
+    if (e >= w) {
+      y += dy;
+      e -= w;
+    }
+  }
+}
+
 void DoSpriteEditorGUI()
 {
     MesaGUI::BeginZone(alh_sprite_editor);
@@ -116,12 +232,9 @@ void DoSpriteEditorGUI()
 //    }
 //    MesaGUI::EditorEndListBox();
 
-    static int brushSz = 1;
     MesaGUI::EditorText("brush size");
     MesaGUI::EditorIncrementableIntegerField("brush size", &brushSz);
 
-    static vec3 activeRGB = vec3();
-    static float activeOpacity = 1.f;
 //    static float userR = 0.f;
 //    static float userG = 0.f;
 //    static float userB = 0.f;
@@ -151,8 +264,8 @@ void DoSpriteEditorGUI()
 
     MesaGUI::EndZone();
 
-    spreditState.frame.w = 32;
-    spreditState.frame.h = 32;
+    spreditState.frame.w = 800;
+    spreditState.frame.h = 600;
     if (spreditState.frame.pixels == 0)
     {
         InitSpriteEditorActionBuffers();
@@ -236,6 +349,10 @@ void DoSpriteEditorGUI()
         Redo(&spreditState);
     }
 
+    static float t;
+    static i32 x0 = -1;
+    static i32 y0 = -1;
+
     static spredit_Color *pixelsBeforeAction = nullptr;
     if (Input.mouseLeftHasBeenPressed && mouseOverViewport)
     {
@@ -251,7 +368,6 @@ void DoSpriteEditorGUI()
         ClearRedoBuffer();
     }
 
-    std::vector<Fuck> restore;
     if (mouseOverViewport)
     {
         // TODO(Kevin): a "pixel has been touched during this action" look up array basically bit array
@@ -263,9 +379,12 @@ void DoSpriteEditorGUI()
 
         i32 x = click.x/zoom;
         i32 y = click.y/zoom;
-        i32 mx = x - brushSz/2;
-        i32 my = y - brushSz/2;
 
+        if (Input.mouseLeftHasBeenPressed)
+        {
+            x0 = x;
+            y0 = y;
+        }
 
         // Eye dropper
         if (Input.KeyHasBeenPressed(SDL_SCANCODE_J))
@@ -282,41 +401,17 @@ void DoSpriteEditorGUI()
             }
         }
 
-
-        float brushRadius = (float)brushSz/2.f;
-
-        for (int i = 0; i < brushSz; ++i)
+        if (Input.mouseLeftPressed)
         {
-            for (int j = 0; j < brushSz; ++j)
-            {
-                float fx = (float)x;
-                float fy = (float)y;
-                if (brushSz % 2 == 1)
-                {
-                    fx += 0.5f;
-                    fy += 0.5f;
-                }
-                float cx = -brushRadius + (float)i + 0.5f;
-                float cy = -brushRadius + (float)j + 0.5f;
-
-                if (sqrtf(float(cx*cx + cy*cy)) < float(brushSz/2) + (brushSz % 2 == 1 ? 0.24f : -0.1f))
-                {
-                    spredit_Color *p = PixelAt(&spreditState.frame, mx+i, my+j);
-                    if (p)
-                    {
-                        // Blend mode: RGB is linearly interpolated, A is added onto where opacity is % from
-                        // existing alpha to 255.
-                        vec3 existingRGB = {RGB255TO1(p->r, p->g, p->b)};
-                        vec3 finalRGB = Lerp(existingRGB, activeRGB, activeOpacity);
-                        restore.push_back({ mx+i, my+j, *p });
-                        p->r = u8(finalRGB.x * 255.f);
-                        p->g = u8(finalRGB.y * 255.f);
-                        p->b = u8(finalRGB.z * 255.f);
-                        p->a = GM_clamp(p->a + u8(float(255 - p->a) * activeOpacity), 0, 255);
-                    }
-                }
-            }
+            algo_line_continuous(x0, y0, x, y);
         }
+        else
+        {
+            DoBrushPoint(x, y);
+        }
+
+        x0 = x;
+        y0 = y;
     }
 
     UpdateGPUTex(&spreditState.frame);
